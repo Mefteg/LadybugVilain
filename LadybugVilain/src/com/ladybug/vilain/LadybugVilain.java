35df037b24256c1f3f695648be2fd3fe00816207
mefteg <gimenez.tom@gmail.com>
2012-12-15 19:16:35 +0100

    Initialisation de LadybugVilain.

--- LadybugVilain/src/com/ladybug/vilain/LadybugVilain.java
+++ LadybugVilain/src/com/ladybug/vilain/LadybugVilain.java
@@ -9,62 +9,24 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.ladybug.engine.game.Game2D;
+import com.ladybug.engine.game.Scene;
 import com.ladybug.engine.gameobject.GameObject;
 
-public class LadybugVilain implements ApplicationListener {
+public class LadybugVilain extends Game2D {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
 	
-	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+	public LadybugVilain() {
+		super();
 		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
+		Scene scene = new Scene();
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		this.addScene(scene);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-		
-		GameObject go = new GameObject();
-		GameObject go2 = new GameObject();
-	}
-
-	@Override
-	public void dispose() {
-		batch.dispose();
-		texture.dispose();
-	}
-
-	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void resume() {
+		GameObject go = new GameObject("Player");
+		scene.addObject(go);
 	}
 }
